62c06217287f8211883be58741bd66d05e06e103
ivanm <ivan.m.g.1791@gmail.com>
2026-02-13 14:20:44 +0100

    variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+vi = 2
+print("vi:",vi,"la , añade automaticamente un espacio")
+print("type(vi):", type(vi))
+vf = 2.0
+print("vi:",vf)
+print("type(vf):", type(vf))
+
+vb = True #Ejemplo de comentario: En Python True/False se escribe la primera en mayusculas
+print("vb:",vb)
+print("type(vb):", type(vb))
+
+vs = "Hola"
+print("vs:",vs)
+print("type(vs):", type(vs))
